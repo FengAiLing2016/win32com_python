052609c48f8b6c6b10a81820ade577a61cec05f6
fengxia <77073000@qq.com>
2017-03-28 23:28:58 +0800

    20170328_3
    20170328_3

--- index.py
+++ index.py
@@ -4,26 +4,27 @@
 # @Author  : Feng_xia
 # @File    : index.py
 
-from utility import sql
-from model import pandas_model,query
-import pandas as pd
 import numpy as np
-from pandas import Series,DataFrame
+import pandas as pd
+
+from model import query
+from utility import sql, pandas_model
 
 
 def main():
 
-    sql_connection=sql.sql_conn('gqserver','CXHBI_010_2014','sa','2016guangQIANG')
-    query_seller=query.query_seller()
-    data=sql_connection.fetchall(query_seller)
+    sql_connection = sql.sql_conn(pandas_model.input_name())
+    query_seller = query.query_seller()
+    data = sql_connection.fetchall(query_seller)
     sql_connection.sql_close()
+
     # 创建pandas的DataFrame
     df = pd.DataFrame(data)
 
     # 转换数据类型
-    df['金额']=df['金额'].astype(float)
+    df['金额'] = df['金额'].astype(float)
 
-    #遍历每列去空格
+    # 遍历每列去空格
     for col_name in df.columns:
         df[col_name] = df[col_name].map(pandas_model.seller_strip)
 
@@ -31,7 +32,7 @@ def main():
     df['品牌'] = df['编号'].map(pandas_model.brand)
 
     # 数据透视表
-    a=pd.pivot_table(df, index=['员工姓名','品牌'],values=['金额'],aggfunc=np.sum)
+    a = pd.pivot_table(df, index=['客户名称'],columns=['员工姓名'], values=['金额'], aggfunc=np.sum,margins=True)
 
     # 导出EXCEL
     a.to_excel('coo.xls', index=True)
--- utility/sql.py
+++ utility/sql.py
@@ -9,33 +9,38 @@ import win32com.client
 
 
 class sql_conn(object):
-    def __init__(self,server_name,data_name,user_name,pwd):
-        self.server_name=server_name
-        self.data_name=data_name
-        self.user_name=user_name
-        self.pwd=pwd
+
+    def __init__(self, *args):
+        server_name,data_name,user_name,pwd = args[0]
+        self.server_name = server_name
+        self.data_name = data_name
+        self.user_name = user_name
+        self.pwd = pwd
         try:
             self.conn_sql = win32com.client.Dispatch('ADODB.Connection')
-            self.conn_sql.Open("Provider=SQLOLEDB;SERVER=%s;DATABASE=%s" % (self.server_name,self.data_name),
-                      self.user_name, self.pwd)
-        except win32com.Error as e:
+            self.conn_sql.Open(
+                "Provider=SQLOLEDB;SERVER=%s;DATABASE=%s" %
+                (self.server_name, self.data_name), self.user_name, self.pwd)
+        except AttributeError as e:
+            print("sql_server Error %d: %s" % (e.args[0], e.args[1]))
+        except pywintypes.com_error as e:
             print("sql_server Error %d: %s" % (e.args[0], e.args[1]))
 
     '''select函数'''
-    def select_sql(self,sql):
+
+    def select_sql(self, sql):
         self.select_Sql = self.conn_sql.Execute(sql)[0]
 
-    #返回SQL数据
-    def fetchall(self,sql):
+    # 返回SQL数据
+    def fetchall(self, sql):
         self.select_sql(sql)
-        value=[]
-        data={}
-        value=self.select_Sql.GetRows()
+        value = []
+        data = {}
+        value = self.select_Sql.GetRows()
         for i in range(len(value)):
-            data[self.select_Sql.Fields(i).Name]=value[i]
-        return  data
+            data[self.select_Sql.Fields(i).Name] = value[i]
+        return data
 
     def sql_close(self):
         self = None
         return self
-
