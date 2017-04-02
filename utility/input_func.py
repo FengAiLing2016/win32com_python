ea6539b5117748593cfb874d6d0a37bba3bbade7
FengAiLing2016 <896287419@qq.com>
2017-04-02 15:53:15 +0800

    20170401_1
    20170401_1

---
+++ utility/input_func.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/4/2 10:08
+# @Author  : Feng_xia
+# @File    : input_func
+
+def input_select():
+    print('''
+    1.输入客户
+    2.输入品牌
+    3.输入业务员
+    4.查询所有
+    ''')
+    input_sel=input()
+    if  input_sel =='1':
